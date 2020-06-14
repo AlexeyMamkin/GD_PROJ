60c72ded60f2a2308729a7aaf6d799d6158fc815
BaJlDuC <32042764+BaJlDuC@users.noreply.github.com>
2020-06-14 23:24:01 +0300

    test commit

---
+++ Assets/Scripts/HelloMorozov.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloMorozov : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello Morozov MN!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
